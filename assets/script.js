1195e8b3ab6cdc0d843d40c651730673a5c44aee
sploganathan <sakthiloganathan1127@gmail.com>
2022-11-20 01:02:15 -0800

    Added function for password length

--- assets/script.js
+++ assets/script.js
@@ -3,11 +3,24 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
   var passwordText = document.querySelector("#password");
+  passwordText.value = "";
+  var password = generatePassword();
 
   passwordText.value = password;
+}
+function generatePassword() {
+  var passwordLength = prompt(
+    "How many characters would you like your password to contain"
+  );
+  if (passwordLength > 8 && passwordLength < 128) {
+    var shouldContinue = prompt(
+      "Click OK to start including special characters"
+    );
 
+    return "You must choose password criteria";
+  }
+  return "Password length should be greater than 8 and less than 128 characters";
 }
 
 // Add event listener to generate button
