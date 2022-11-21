66d62e3abee16a6061d43ea9c7bd8903518613e3
sploganathan <sakthiloganathan1127@gmail.com>
2022-11-21 10:52:10 -0800

    Added comments

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,31 @@
 // Assignment Code
+// Getting button element using queryselector
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  // To display the password we are queryselecting the password text area
   var passwordText = document.querySelector("#password");
-  passwordText.value = "";
+  // passwordText.value = "";
+  // Password generating logic goes here
   var password = generatePassword();
 
   passwordText.value = password;
 }
+
 function generatePassword() {
+  // Setting password length we are getting in this variable 'passwordlength'
   var passwordLength = prompt(
     "How many characters would you like your password to contain"
   );
-  if (passwordLength > 8 && passwordLength < 128) {
+  // Condition - Password length should be greater than or equal to 8 and less than or equal to 128.
+  if (passwordLength >= 8 && passwordLength <= 128) {
     var shouldContinue = prompt(
       "Click OK to start including special characters"
     );
+    // If the above condition is true it enters the below if loop or if it is false it goes to line 48
     if (shouldContinue) {
+      // Storing the password criterias in the respective varaiables
       var lowerCase = prompt(
         "Click OK to confirm including lowercase characters"
       );
@@ -28,6 +36,7 @@ function generatePassword() {
       var specialCharacter = prompt(
         "Click OK to confirm including special characters"
       );
+      // If the user hasn't choosen anyone of the password criteria it again triggers the 'generatepassword' function
       if (!lowerCase && !upperCase && !numeric && !specialCharacter) {
         alert("Please choose atleast one password criteria");
         generatePassword();
